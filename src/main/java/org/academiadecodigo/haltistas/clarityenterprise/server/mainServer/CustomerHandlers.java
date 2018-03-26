01c4f16646552c1997713bcfba22e076440dcbcf
tiago.fareleiro <tiago_f93@hotmail.com>
2018-03-26 21:43:35 +0100

    added method and logicIdea for connection, next step executor

--- src/main/java/org/academiadecodigo/haltistas/clarityenterprise/server/mainServer/CustomerHandlers.java
+++ src/main/java/org/academiadecodigo/haltistas/clarityenterprise/server/mainServer/CustomerHandlers.java
@@ -1,4 +1,31 @@
 package org.academiadecodigo.haltistas.clarityenterprise.server.mainServer;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+
 public class CustomerHandlers {
+
+
+    public static void main(String[] args) {
+        try {
+            Socket socketToServer = new Socket("localhost", 6000);
+            Executors.newCachedThreadPool();
+
+
+            PrintWriter msgToServer = new PrintWriter(socketToServer.getOutputStream(),true);
+
+            while (true) {
+
+                //logic in web when cusotmer access it case all relevant information is sent to Server in which will
+                //redirect and request case to department handling"
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/org/academiadecodigo/haltistas/clarityenterprise/server/mainServer/MainServer.java
+++ src/main/java/org/academiadecodigo/haltistas/clarityenterprise/server/mainServer/MainServer.java
@@ -1,4 +1,35 @@
 package org.academiadecodigo.haltistas.clarityenterprise.server.mainServer;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MainServer {
+
+    public static void main(String[] args) {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(6000);
+            Socket socket = serverSocket.accept();
+
+
+            BufferedReader receivedConnection = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            while (true) {
+
+                String readConnection = receivedConnection.readLine();
+
+                //when accessed in online account and accessed trough a click it will than access to the information requested
+
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
